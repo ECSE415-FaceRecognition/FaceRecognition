9d7ba946dc130d0aef8e2b1d1e5a09bac0fc1deb
Jit <kanetkarster@gmail.com>
2016-04-11 10:07:52 -0400

    added some comments

--- ECSE415-Project/main.cpp
+++ ECSE415-Project/main.cpp
@@ -14,6 +14,8 @@
 // allow numeric limits to work
 #undef max
 
+const int NUM_FOLDS = 7;
+
 void do_lbp_face_recognition(std::vector<std::string> const& people);
 void seven_fold_cv(std::vector<std::string> &people, std::vector<std::vector<cv::string>> &folds);
 void qmul_all_images_of_person(std::string person);
@@ -52,8 +54,8 @@ void do_lbp_face_recognition(std::vector<std::string> const& people) {
 	std::vector<std::vector<cv::string>> image_names =  open_all_qmul_by_person(people);
 	std::vector<std::vector<cv::string>> folds;
 
-	/* split into 7 training sets */
-	folds.resize(7);
+	/* split into 7 training sets, preserving the order of people */
+	folds.resize(NUM_FOLDS);
 	for (int i=0; i < image_names.size(); i++) {
 		srand(time(0));
 		seven_fold_cv(image_names[i], folds);
@@ -61,7 +63,9 @@ void do_lbp_face_recognition(std::vector<std::string> const& people) {
 
 	std::vector<std::vector<cv::string>> training_images;
 	training_images.resize(people.size());
-	for (int fold=0; fold < 6; fold++) {
+
+	/* create a set of training images from 6 of the 7 folds */
+	for (int fold=0; fold < (NUM_FOLDS-1); fold++) {
 		int i=0;
 		for (int person=0; person<people.size(); person++) {
 			while ( i < folds[fold].size() && (folds[fold][i].substr(5, people[person].size()) == people[person].substr(0, people[person].size()))) {
@@ -70,19 +74,29 @@ void do_lbp_face_recognition(std::vector<std::string> const& people) {
 			}
 		}
 	}
+
+	/* run lbp recognition for a single set of folds */
 	for (int levels = 1; levels <= 10; levels++) {
+		/* clear histograms from last session */
 		histograms.clear();
 		int guessed_correct = 0;
+
+		/* train on this set of training subsamples */
 		lbp_train(training_images, histograms, levels);
 
-		for (int i=0; i < folds[6].size(); i++) {
+		/* run recognition for testing subsample */
+		for (int i=0; i < folds[(NUM_FOLDS-1)].size(); i++) {
 			std::string test_person = folds[6][i];
 			std::string guessed = lbp_test(test_person, people, histograms, levels);
-			//std::cout << "Guessed: " << guessed << ", " << "Actual person was: " << test_person << std::endl;
+			
+			/* determine if lbp guessed properly. test_person is a file name, so we simply
+			   search in the file name for the guessed person */
 			if (test_person.find(guessed) != std::string::npos) {
 				guessed_correct++;
 			}
 		}
+
+		/* in order to log the recognition rate */
 		std::cout << "for " << levels << " guessed with a rate of " << (((double) guessed_correct)/ ((double) folds[6].size())) << std::endl;
 	}
 
@@ -106,10 +120,14 @@ void qmul_all_images_of_person(std::string person) {
 }
 
 void seven_fold_cv(std::vector<std::string> &people, std::vector<std::vector<cv::string>> &folds) {
+
+	/* randomize the people vector */
 	std::random_shuffle(people.begin(), people.end());
+
+	/* split people into the 7 subsamples */
 	int i = people.size();
 	while(i > 0) {
 		i--;
-		folds[i%7].push_back(people[i]);
+		folds[i%NUM_FOLDS].push_back(people[i]);
 	}
 }
